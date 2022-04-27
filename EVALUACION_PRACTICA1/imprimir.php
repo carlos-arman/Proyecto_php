a95eb3f043ab2e9e6d63df61b51d94e189dad81e
carlos-arman <minerocarlos28@gmail.com>
2022-04-27 16:14:59 -0700

    ARCHIVO PHP MODIFICADO

--- EVALUACION_PRACTICA1/imprimir.php
+++ EVALUACION_PRACTICA1/imprimir.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    //IMPRIMIR DATOS
 <?php
 $nombre1 = $_POST['nombre']; 
 $fecha = $_POST['fecha'];
